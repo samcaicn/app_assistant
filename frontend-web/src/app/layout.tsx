a7fc7e8eaf5249ec9c97765602da7f9a97810461
markshawn2020 <shawninjuly@gmail.com>
2024-08-22 18:09:02 +0800

    updated swot

--- frontend-web/src/app/layout.tsx
+++ frontend-web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { GlobalProvider } from "@/app/global-provider"
+import { SwotIcoWhite } from "@/components/assets"
 
 import { Dev } from "@/components/dev"
 import { SwotBanner } from "@/components/swot-banner"
@@ -20,7 +21,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "飞脑",
   description: "更像真人的AI社交助理",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: "icon", url: SwotIcoWhite.src }],
 
   // 不加这个会导致微信公众号文章图片无法引用，ref: https://blog.csdn.net/qq_42961150/article/details/121833692#:~:text=%E8%AF%A6%E8%A7%A3%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87%E9%98%B2%E7%9B%97%E9%93%BE,%E6%8E%A5%E5%8F%A3%EF%BC%8C%E8%AE%A9%E6%88%91%E4%BB%AC...
   referrer: "no-referrer",
--- frontend-web/src/components/assets.ts
+++ frontend-web/src/components/assets.ts
@@ -1,1 +1,2 @@
 export { default as SwotLogoWhite } from "@/branding/swot/swot_logo_white.png"
+export { default as SwotIcoWhite } from "@/branding/swot/swot_logo_white.ico"
