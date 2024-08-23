4c29562e7f6bdd156e815695b21150d9e6ef930e
markshawn2020 <shawninjuly@gmail.com>
2024-08-24 00:09:46 +0800

    added mini

---
+++ frontend-web/src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { getWechatAuthorizationUrl } from "@cs-magic/common/dist/auth/index.js"
+import { FlexContainer } from "@cs-magic/react/components/flex-container"
+import QRCode from "qrcode.react"
+
+export default function HomePAge() {
+  const authorizationUrl = getWechatAuthorizationUrl()
+  console.log(authorizationUrl)
+
+  return (
+    <FlexContainer>
+      <QRCode size={256} value={authorizationUrl} />
+    </FlexContainer>
+  )
+}
