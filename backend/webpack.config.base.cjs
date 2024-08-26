669c52bc0967b691ef8cb1c821661bf02e3703cd
markshawn2020 <shawninjuly@gmail.com>
2024-08-27 00:18:17 +0800

    :rocket: fixed webpack build

--- backend/webpack.config.base.cjs
+++ backend/webpack.config.base.cjs
@@ -1,4 +1,5 @@
 const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   target: "node", // ref: https://chatgpt.com/c/0bb5bb00-68a9-4c82-b396-0d1f4224215d
@@ -10,17 +11,25 @@ module.exports = {
     path: path.resolve("/tmp", "dist"),
   },
 
+  // 需要保留这些，否则依赖库（例如 wechaty-grpc（这个不建议用CopyPlugin，因为还有依赖））里用了这些会使它们在新的输出位置运行
+  node: {
+    __dirname: true,
+    // __filename: true, // 本项目可不需要
+    // global: true, // 本项目可不需要
+  },
+
   externals: {
     "playwright-core": "commonjs playwright-core",
-    // "wechaty-grpc": "commonjs wechaty-grpc", // dynamic import error: ../../out
-    // "wechaty-puppet-service": "commonjs wechaty-puppet-service", // native module error: level
-    // "wechaty-puppet-wechat4u": "commonjs wechaty-puppet-wechat4u", // o.w. Failed to import Wechaty Puppet Provider (WPP) NPM module: "wechaty-puppet-wechat4u"
   },
 
-  resolve: {
-    alias: {
-      '../../out': path.resolve(__dirname, '../../node_modules/wechaty-grpc/out')
-    },
-  },
-
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "../../node_modules/playwright-core",
+          to: "node_modules/playwright-core",
+        },
+      ],
+    }),
+  ],
 }
