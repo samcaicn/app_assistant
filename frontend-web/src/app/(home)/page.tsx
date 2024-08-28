a73d490c950dd81d49912be276746b37c544a9a8
markshawn2020 <shawninjuly@gmail.com>
2024-08-28 22:07:15 +0800

    ""

--- frontend-web/src/app/(home)/page.tsx
+++ frontend-web/src/app/(home)/page.tsx
@@ -1,7 +1,3 @@
-"use client"
-
-import { FlexContainer } from "@cs-magic/react/components/flex-container"
-
-export default function HomePAge() {
-  return <FlexContainer>hello</FlexContainer>
+export default function HomePage() {
+  return "hello"
 }
