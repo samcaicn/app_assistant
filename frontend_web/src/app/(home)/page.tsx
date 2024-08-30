5360164ada4b368cbe9a68eb7fa9ee73226c64ab
markshawn2020 <shawninjuly@gmail.com>
2024-08-30 14:37:00 +0800

    added turbo animation

--- frontend_web/src/app/(home)/page.tsx
+++ frontend_web/src/app/(home)/page.tsx
@@ -1,12 +1,14 @@
 import { SwotLogoWhite } from "@/components/assets";
 import { FlexContainer } from "@/packages_frontend/react/dist/components/flex-container";
+import BackgroundLines from "@/packages_frontend/react/dist/components/背景条纹动画/components/background-lines";
 import { Button } from "@/packages_frontend/shadcn/dist/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <FlexContainer orientation={"vertical"} className={"!gap-8"}>
+    // <BackgroundLines>
+    <FlexContainer orientation={"vertical"} className={"!gap-8 h-full"}>
       <div className={"grid grid-cols-[10] grid-rows-[120]"} />
       <h2 className={"text-[64px] gradient-1"}>Your AI Knowledge Companion</h2>
       <div
@@ -41,6 +43,9 @@ export default function HomePage() {
       </div>
 
       <Image src={SwotLogoWhite} alt={"swot"} width={256} height={256} />
+
+      <div className={"bg-indigo-500 w-full h-20"} />
     </FlexContainer>
+    // </BackgroundLines>
   );
 }
