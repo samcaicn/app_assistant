46e1ab05b5b1d465e4d99d8ca84d3e452577dc69
markshawn2020 <shawninjuly@gmail.com>
2024-09-26 22:39:03 +0800

    .

--- frontend_common/src/components/footer.tsx
+++ frontend_common/src/components/footer.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { ImageEqualHeight } from "@cs-magic/react/components/image-equal-height"
+import { ImageEqualHeight } from "@cs-magic/react/components/image-equal-height";
 
-import CsMagicBannerSvg from "@/branding/CS-magic_banner_方正字汇-锐速体.svg"
+import CsMagicBannerSvg from "../../../../assets/branding/CS-magic_banner_方正字汇-锐速体.svg";
 
 export const Footer = () => {
-  const enterprises: string[] = []
+  const enterprises: string[] = [];
 
   return (
     <div className={"mt-auto flex shrink-0 flex-col items-center gap-2 p-4"}>
       {/*  founders */}
-      <div className={"mt-auto flex h-4 w-full items-center justify-center gap-4 sm:h-8"}>
-        <Link href={"https://cs-magic.cn"} className={"h-full"} target={"_blank"}>
+      <div
+        className={
+          "mt-auto flex h-4 w-full items-center justify-center gap-4 sm:h-8"
+        }
+      >
+        <Link
+          href={"https://cs-magic.cn"}
+          className={"h-full"}
+          target={"_blank"}
+        >
           <CsMagicBannerSvg className={"h-full w-auto"} />
         </Link>
 
@@ -28,5 +36,5 @@ export const Footer = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
